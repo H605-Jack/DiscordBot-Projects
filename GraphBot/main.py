daa13921d29f8fcc302f87f8ecc15567f9e6bfbf
Kiet <giakiet609@gmail.com>
2024-06-26 23:17:09 -0400

    modified:   GraphBot/main.py

--- GraphBot/main.py
+++ GraphBot/main.py
@@ -10,7 +10,7 @@ intent.members = True
 intent.message_content = True
 bot = commands.Bot(command_prefix="g!", intents=intent)
 
-async def load_file(dir: str) -> None:
+async def load_dir(dir: str) -> None:
   for filename in os.listdir(f"cogs/{dir}"):
     if filename.endswith(".py"):
       await bot.load_extension(f"cogs.{dir}.{filename[:-3]}")
@@ -20,15 +20,19 @@ async def load_file(dir: str) -> None:
 async def on_ready():
   print("Bot is ready.")
 
-# runtime
+# initiate runtime
 async def main():
-  await asyncio.gather(
-    load_file("global"),
-    load_file("listening"),
-    bot.start(os.getenv("TOKEN"))
-  )
-
+  await load_dir("global"),
+  await load_dir("listening"),
+  await bot.start(os.getenv("TOKEN"))
+  
+# runtime
+loop = asyncio.new_event_loop()
 try:
-  asyncio.run(main())
+  loop.run_until_complete(main())
 except KeyboardInterrupt as e:
-  print("Exception")
+  # Termination requested
+  print("\nTermination Requested. Bot is shutting down...")
+  if not loop.is_closed():
+    loop.close()
+    print("Bot has successfully disconnected.")
